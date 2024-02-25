46d5150ce93158242aaee0200ce2158023a12e98
kento980037 <kento_0037@yahoo.co.jp>
2024-02-25 19:49:35 +0900

    add tag

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ const typeDefs = `
     GRAPHIC
   }
 
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
+  }
+
   type Photo {
     id: ID!
     url: String!
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
+    taggedUsers: [User!]!
   }
 
   input PostPhotoInput {
@@ -34,7 +44,60 @@ const typeDefs = `
 `;
 
 let _id = 0
-let photos = []
+let users = [
+    {
+      githubLogin: "mHattrup",
+      name: "Mike Hattrup",
+    },
+    {
+      githubLogin: "gPlake",
+      name: "Glen Plake",
+    },
+    {
+      githubLogin: "sSchmidt",
+      name: "Scot Schmidt",
+    },
+  ]
+let photos = [
+    {
+      id: 1,
+      name: "Dropping the Heart Chute",
+      description: "The heart chute is one of my favorite chutes",
+      category: "ACTION",
+      githubUser: "gPlake",
+    },
+    {
+      id: 2,
+      name: "Enjoying the sunshine",
+      category: "SELFIE",
+      githubUser: "sSchmidt",
+    },
+    {
+      id: 3,
+      name: "Gunbarrel 25",
+      description: "25 laps on gunbarrel today",
+      category: "LANDSCAPE",
+      githubUser: "sSchmidt",
+    },
+  ]
+  let tags = [
+    {
+        "photoID": 1, 
+        "userID": "gPlake",
+    },
+    {
+        "photoID": 2, 
+        "userID": "sSchmidt",
+    },
+    {
+        "photoID": 2, 
+        "userID": "mHattrup",
+    },
+    {
+        "photoID": 2, 
+        "userID": "gPlake",
+    },
+  ]
 
 const resolvers = {
   Query: {
@@ -54,7 +117,18 @@ const resolvers = {
   },
 
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+        return users.find(u => u.githubLogin === parent.githubUser)
+    },
+    taggedUsers: parent => tags.filter(tag => tag.photoID === parent.id).map(tag => tag.userID).map(userID => users.find(u => u.githubLogin === userID))
+  },
+
+  User: {
+    postedPhotos: parent => {
+        return photos.filter(p => p.githubLogin === parent.githubLogin)
+    },
+    inPhotos: parent => tags.filter(tag => tag.userID === parent.id).map(tag => tag.photoID).map(photoID => photos.find(p => p.id === photoID))
   }
 };
 
